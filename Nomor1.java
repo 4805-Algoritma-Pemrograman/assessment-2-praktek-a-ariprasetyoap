be208f99fca77b8bde57a4b178b201f028aa5d4f
ariprasetyoap <ariprasetyo34728@gmail.com>
2024-12-12 09:20:07 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,25 +1,18 @@
-
 import java.util.Scanner;
 
-public class no1 {
-    public static void tampilkanBilanganGanjil(int n) {
-        System.out.println("Bilangan ganjil dari 0 hingga " + n + " adalah:");
-        for (int i = 0; i <= n; i++) {
-            if (i % 2 != 0) {
-                System.out.print(i + " ");
-            }
-        }
-        System.out.println(); 
+public class Nomor1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan bilangan n: ");
+        int n = scanner.nextInt();
+        tampilkanGanjil(n);
     }
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        
-        System.out.print("Masukkan bilangan n: ");
-        int n = input.nextInt();
-        
-        tampilkanBilanganGanjil(n);
-        
-        input.close();
+    public static void tampilkanGanjil(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
     }
 }

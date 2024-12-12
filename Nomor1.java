5405cb3f35ef2adc359b65b07fded247c5e6db49
ariprasetyoap <ariprasetyo34728@gmail.com>
2024-12-12 08:26:13 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
 public class Nomor1 {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan bilangan n: ");
+        int n = scanner.nextInt();
+        tampilkanGanjil(n);
     }
-    
+
+    public static void tampilkanGanjil(int n) {
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
 }
